3a747077120e6c4f61be975c59ac6755056244e2
Stratadox <jesse@stratadox.com>
2018-02-11 23:39:23 +0100

    Move property analysis construction into the test

--- tests/Unit/PropertyAnalysis_contains_information_on_the_property.php
+++ tests/Unit/PropertyAnalysis_contains_information_on_the_property.php
@@ -16,25 +16,26 @@ class PropertyAnalysis_contains_information_on_the_property extends TestCase
     /**
      * @test
      * @dataProvider scalarTypes
-     * @param string           $scalarType
-     * @param PropertyAnalysis $property
+     * @param string $scalarType
      */
-    function having_a_property_type_but_no_element_type_for(
-        string $scalarType,
-        PropertyAnalysis $property
-    )
+    function having_a_property_type_but_no_element_type_for(string $scalarType)
     {
-        $this->assertSame($scalarType, $property->type());
-        $this->assertNull($property->elementType());
+        $this->assertSame(
+            $scalarType,
+            PropertyAnalysis::forType($scalarType)->type()
+        );
+        $this->assertNull(
+            PropertyAnalysis::forType($scalarType)->elementType()
+        );
     }
 
     public function scalarTypes() : array
     {
         return [
-            'string' => ['string', PropertyAnalysis::forType('string')],
-            'int' => ['int', PropertyAnalysis::forType('int')],
-            'bool' => ['bool', PropertyAnalysis::forType('bool')],
-            'Foo' => [Foo::class, PropertyAnalysis::forType(Foo::class)],
+            'string' => ['string'],
+            'int' => ['int'],
+            'bool' => ['bool'],
+            'Foo' => [Foo::class],
         ];
     }
 }
