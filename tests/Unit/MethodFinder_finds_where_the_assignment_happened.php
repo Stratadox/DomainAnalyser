f840c3ba38f9b5df205afe83170d92a6ed207dd4
Stratadox <jesse@stratadox.com>
2018-02-13 00:26:13 +0100

    Add test for finding the method from inside a condition

--- tests/Unit/MethodFinder_finds_where_the_assignment_happened.php
+++ tests/Unit/MethodFinder_finds_where_the_assignment_happened.php
@@ -8,7 +8,9 @@ use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Param;
+use PhpParser\Node\Scalar\LNumber;
 use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\If_;
 use PHPUnit\Framework\TestCase;
 use Stratadox\DomainAnalyser\MethodFinder;
 
@@ -33,6 +35,36 @@ class MethodFinder_finds_where_the_assignment_happened extends TestCase
         ]);
 
         $assignment->setAttribute('parent', $method);
+
+        $finder = new MethodFinder;
+
+        $this->assertSame($method, $finder->methodOfThe($assignment));
+    }
+
+    /** @test */
+    function finding_the_method_when_in_a_condition()
+    {
+        $method = new ClassMethod('foo', [
+            'params' => [
+                new Param('foo', null, 'string')
+            ],
+            'stmts' => [
+                $condition = new If_(new LNumber(1),
+                    [
+                        'stmts' => [
+                            $assignment = new Assign(
+                                new PropertyFetch(new Variable('this'), 'foo'),
+                                new Variable('foo')
+                            )
+                        ]
+                    ]
+                ),
+
+            ]
+        ]);
+
+        $assignment->setAttribute('parent', $condition);
+        $condition->setAttribute('parent', $method);
 
         $finder = new MethodFinder;
 
