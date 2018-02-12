4ba4db30745178a59fb897ea279f1cc70a773c9d
Stratadox <jesse@stratadox.com>
2018-02-13 00:15:41 +0100

    Make tested method follow correct structure

--- tests/Unit/ParentConnector_adds_parental_relationships.php
+++ tests/Unit/ParentConnector_adds_parental_relationships.php
@@ -7,6 +7,7 @@ namespace Stratadox\DomainAnalyser\Test\Unit;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Param;
 use PhpParser\Node\Stmt\ClassMethod;
 use PHPUnit\Framework\TestCase;
 use Stratadox\DomainAnalyser\ParentConnector;
@@ -19,11 +20,16 @@ class ParentConnector_adds_parental_relationships extends TestCase
     /** @test */
     function foo()
     {
-        $method = new ClassMethod('foo', [
-            $assignment = new Assign(
-                new PropertyFetch(new Variable('this'), 'foo'),
-                new Variable('foo')
-            )
+        $method = new ClassMethod('setFoo', [
+            'params' => [
+                new Param('foo', null, 'string')
+            ],
+            'stmts' => [
+                $assignment = new Assign(
+                    new PropertyFetch(new Variable('this'), 'foo'),
+                    new Variable('foo')
+                )
+            ]
         ]);
 
         $this->assertNull($assignment->getAttribute('parent'));
