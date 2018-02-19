fff446d9f8b710f5e90620b1966229550879c34a
Stratadox <jesse@stratadox.com>
2018-02-20 00:51:22 +0100

    Refactor down to the root test by moving method inline

--- tests/Unit/SyntaxTree/DownToTheRoot_traverses_down_from_a_starting_node.php
+++ tests/Unit/SyntaxTree/DownToTheRoot_traverses_down_from_a_starting_node.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Stratadox\DomainAnalyser\Test\Unit\SyntaxTree;
 
-use PhpParser\Node;
 use PhpParser\Node\Expr\Variable;
 use PHPUnit\Framework\TestCase;
 use Stratadox\DomainAnalyser\SyntaxTree\DownToTheRoot;
@@ -17,9 +16,9 @@ class DownToTheRoot_traverses_down_from_a_starting_node extends TestCase
     /** @test */
     function traversing_through_all_the_previous_nodes()
     {
-        $node[2] = $this->someNode();
-        $node[1] = $this->someNode($node[2]);
-        $node[0] = $this->someNode($node[1]);
+        $node[2] = new Variable('foo');
+        $node[1] = new Variable('foo', ['prev' => $node[2]]);
+        $node[0] = new Variable('foo', ['prev' => $node[1]]);
 
         $traversal = DownToTheRoot::startingAt($node[0]);
 
@@ -33,11 +32,11 @@ class DownToTheRoot_traverses_down_from_a_starting_node extends TestCase
     /** @test */
     function asking_the_parent_when_out_of_siblings()
     {
-        $node[4] = $this->someNode();
-        $node[3] = $this->someNode($node[4]);
-        $node[2] = $this->someNode(null, $node[3]);
-        $node[1] = $this->someNode($node[2]);
-        $node[0] = $this->someNode($node[1]);
+        $node[4] = new Variable('foo');
+        $node[3] = new Variable('foo', ['prev' => $node[4]]);
+        $node[2] = new Variable('foo', ['parent' => $node[3]]);
+        $node[1] = new Variable('foo', ['prev' => $node[2]]);
+        $node[0] = new Variable('foo', ['prev' => $node[1]]);
 
         $traversal = DownToTheRoot::startingAt($node[0]);
 
@@ -46,15 +45,5 @@ class DownToTheRoot_traverses_down_from_a_starting_node extends TestCase
         foreach ($traversal as $atCurrentIndex => $actualNode) {
             $this->assertSame($node[$atCurrentIndex], $actualNode);
         }
-    }
-
-    private function someNode(
-        Node $previous = null,
-        Node $parent = null
-    ): Node {
-        return new Variable('foo', [
-            'prev' => $previous,
-            'parent' => $parent,
-        ]);
     }
 }
