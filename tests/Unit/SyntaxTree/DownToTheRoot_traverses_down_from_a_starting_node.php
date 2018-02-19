931e2e1dcfce311f87e2f91f845964c6f57c7f41
Stratadox <jesse@stratadox.com>
2018-02-20 00:44:57 +0100

    Include an assertion for parental visits in down to the root test

--- tests/Unit/SyntaxTree/DownToTheRoot_traverses_down_from_a_starting_node.php
+++ tests/Unit/SyntaxTree/DownToTheRoot_traverses_down_from_a_starting_node.php
@@ -30,10 +30,31 @@ class DownToTheRoot_traverses_down_from_a_starting_node extends TestCase
         }
     }
 
-    private function someNode(Node $previous = null) : Node
+    /** @test */
+    function asking_the_parent_when_out_of_siblings()
     {
+        $node[4] = $this->someNode();
+        $node[3] = $this->someNode($node[4]);
+        $node[2] = $this->someNode(null, $node[3]);
+        $node[1] = $this->someNode($node[2]);
+        $node[0] = $this->someNode($node[1]);
+
+        $traversal = DownToTheRoot::startingAt($node[0]);
+
+        $this->assertCount(5, $traversal);
+
+        foreach ($traversal as $atCurrentIndex => $actualNode) {
+            $this->assertSame($node[$atCurrentIndex], $actualNode);
+        }
+    }
+
+    private function someNode(
+        Node $previous = null,
+        Node $parent = null
+    ): Node {
         return new Variable('foo', [
             'prev' => $previous,
+            'parent' => $parent,
         ]);
     }
 }
